cae872447616f842043d50814846f1e5e797b43e
AbhishekUS5 <abhishekshet12@gmail.com>
2026-01-05 09:48:42 +0530

    This

--- Day 13 this,bind and apply/index.js
+++ Day 13 this,bind and apply/index.js
@@ -1,3 +1,4 @@
+"use strict";
 console.log("Day 13. The this keyword");
 // purpose : it can helo us nderstand what exactly happenign in the particualr execution context
 // this means pointing to something
@@ -9,14 +10,54 @@ console.log("This at the global", this); // window
 
 // Binding
 // Inside of an object - implicit binding
-// ay in whioch you understand that is if a method is called on an object using the dot notation, the context of this is bound or associated to the object on which we have invoked
-const employee ={
-    id:"A5778",
+// way in which you understand that is if a method is called on an object using the dot notation, the context of this is bound or associated to the object on which we have invoked
+const employee = {
+    id: "A5778",
     firstName: "Alex",
     lastName: "B",
-     returnThis: function () {
+    returnThis: function () {
         return this;
-     }
+    },
+    getFullName: function () {
+        return `${this.firstName} ${this.lastName}`
+    }
 }
 console.log(employee.id);
+console.log("this value", employee.returnThis()); // outputs the object
 console.log("this value", employee.returnThis());
+console.log("constructed full name using this", employee.getFullName());
+
+const tom = {
+    name: "Tom",
+    age: 7
+}
+const jerry = {
+    name: "Jerry",
+    age: 7
+}
+function greetMe(obj) {
+    obj.logMessage = function () {
+        console.log(`${this.name} is ${this.age} years old`)
+    }
+}
+greetMe(jerry);
+jerry.logMessage();
+
+greetMe(tom);
+tom.logMessage();
+
+//inside function
+function sayname() {
+    console.log("this inside a function", this)
+}
+sayname();
+
+function outer(a) {
+    console.log("this inside an outer function", this);
+    return function inner(b) {
+        console.log("this inside an inner function", this);
+    }
+}
+const outerResult = outer(5);
+outerResult(3);
+
